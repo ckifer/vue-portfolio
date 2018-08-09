a389f2be67fd01f56ece95ae3ff32c91efb22b9c
Coltin Kifer <Coltin.Kifer@kennametal.com>
2018-08-09 13:56:17 -0400

    initial commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-footer :fixed="fixed" app>
+        <span>&copy; @{{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheFooter'
+    }
+</script>
+
+<style scoped>
+
+</style>
